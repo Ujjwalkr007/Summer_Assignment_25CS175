7f123e3f34d875c7f1b46e20b13c0101ee91e26c
Ujjwal Kumar Tripathi <ujjwaltripathi073@gmail.com>
2026-06-04 20:02:48 +0530

    Add files via upload

---
+++ day1/ques1.py
@@ -0,0 +1,7 @@
+n = 10 
+total_sum = 0
+
+for i in range(1,n+1):
+    total_sum= total_sum+i
+
+    print("the sum is:",total_sum)
---
+++ day1/ques2.py
@@ -0,0 +1,5 @@
+no = 8
+
+for i in range(1,11):
+    result = no *i
+    print(no,"x", "=", result)
