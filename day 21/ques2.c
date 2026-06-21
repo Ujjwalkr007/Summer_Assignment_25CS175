7853a2c9e2bcc8e2c8af4e8b499cc4389964e02d
Ujjwal Kumar Tripathi <ujjwaltripathi073@gmail.com>
2026-06-21 22:22:24 +0530

    Add files via upload

---
+++ day 21/ques2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    char str[100], temp;
+    int i, length = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while(str[length] != '\0' && str[length] != '\n') {
+        length++;
+    }
+
+    for(i = 0; i < length / 2; i++) {
+        temp = str[i];
+        str[i] = str[length - 1 - i];
+        str[length - 1 - i] = temp;
+    }
+
+    printf("Reversed string: %s", str);
+
+    return 0;
+}
